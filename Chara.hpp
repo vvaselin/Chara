3481f2fc0573a6f14c0b42d8b951eeb027b3287b
vvaselin <87738472+vvaselin@users.noreply.github.com>
2024-01-30 12:17:22 +0900

    Update Chara.hpp

--- Chara.hpp
+++ Chara.hpp
@@ -13,7 +13,10 @@ public:
 
 	// シリアライズに対応させるためのメンバ関数を定義する
 	template <class Archive>
-	void SIV3D_SERIALIZE(Archive& archive);
+	void SIV3D_SERIALIZE(Archive& archive)
+	{
+		archive(m_name, m_lv);
+	}
 
 private:
 	String m_name;
